928400582d9ece326a326e5e32da1c35cb2ad591
pogeum <143607484+pogeum@users.noreply.github.com>
2023-11-14 15:30:01 +0900

    member엔í티티랑 레포지터리까지 만듦 (#22)

--- src/main/java/com/TeamProject/TeamProject/Member/Member.java
+++ src/main/java/com/TeamProject/TeamProject/Member/Member.java
@@ -1,12 +1,29 @@
 package com.TeamProject.TeamProject.Member;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String membername; //아이디
+
+    private String password; // 비번
+
+    @Column(unique = true)
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime createDate; //가입일시
 
 }
---
+++ src/main/java/com/TeamProject/TeamProject/Member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.TeamProject.TeamProject.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
