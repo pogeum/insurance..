f3071c1cd9aa12d8a699c0dacb796072dd4e7c09
sang5517 <143565832+sang5517@users.noreply.github.com>
2023-11-14 14:57:07 +0900

    수정

--- src/main/java/com/TeamProject/TeamProject/Review/Review.java
+++ src/main/java/com/TeamProject/TeamProject/Review/Review.java
@@ -1,4 +1,12 @@
 package com.TeamProject.TeamProject.Review;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 }
