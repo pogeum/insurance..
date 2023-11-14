fa89c2662f1362cedbe5b8f710406f9b0348e883
pogeum <143607484+pogeum@users.noreply.github.com>
2023-11-14 16:53:14 +0900

    ãmemberservice++ (#35)

--- src/main/java/com/TeamProject/TeamProject/Member/MemberRepository.java
+++ src/main/java/com/TeamProject/TeamProject/Member/MemberRepository.java
@@ -2,5 +2,8 @@ package com.TeamProject.TeamProject.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findBymembername(String membername);
 }
---
+++ src/main/java/com/TeamProject/TeamProject/Member/MemberService.java
@@ -0,0 +1,37 @@
+package com.TeamProject.TeamProject.Member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    public final MemberRepository memberRepository;
+
+    public Member createMember(String membername, String password, String email) {
+        Member member = new Member();
+        member.setMembername(membername);
+        member.setEmail(email);
+        member.setPassword(password);
+        member.setCreateDate(LocalDateTime.now());
+        this.memberRepository.save(member);
+        return member;
+    }
+
+    public Member getMember(String membername) {
+        Optional<Member> member = this.memberRepository.findBymembername(membername);
+        return member.get();
+
+//        if (member.isPresent()) {
+//            return member.get();
+//        } else {
+//            throw new DataNotFoundException("회원이 없습니다.");
+//        }
+    }
+
+
+}
