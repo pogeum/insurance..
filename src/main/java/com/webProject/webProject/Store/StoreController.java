4f8764d5c776e38e124284adecc0cc79282b7a1e
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-21 13:43:07 +0900

    store_list 수정

--- src/main/java/com/webProject/webProject/Store/StoreController.java
+++ src/main/java/com/webProject/webProject/Store/StoreController.java
@@ -1,11 +1,16 @@
 package com.webProject.webProject.Store;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,7 +20,7 @@ import java.util.List;
 public class StoreController {
 
     private final StoreRepository storeRepository;
-
+    private final StoreService storeService;
     @GetMapping("/list")
     public String list(Model model){
         List<Store> storeList = this.storeRepository.findAll();
@@ -34,4 +39,17 @@ public class StoreController {
 
         return "store_form";
     }
+
+    @PostMapping("/mylocation") // POST 요청을 처리하기 위한 애노테이션 추가
+    public String getMyLocation(Model model, @RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude) {
+        // 클라이언트의 위치 정보를 가져오는 메서드 호출 (이 부분은 getLocationFromCoordinates 메서드를 호출하는 것으로 가정)
+        String location = this.storeService.getLocationFromCoordinates(latitude, longitude);
+
+        List<Store> storeList = this.storeRepository.findAll();
+        model.addAttribute("storeList", storeList);
+        model.addAttribute("location", location);
+
+        return "store_list";
+    }
+
 }
