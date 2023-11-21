e1292b37bbbce52550616f8f8b6fed9876aebd39
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-21 10:18:56 +0900

    store_list 기능 및 UI 개발

--- src/main/java/com/webProject/webProject/Store/StoreController.java
+++ src/main/java/com/webProject/webProject/Store/StoreController.java
@@ -1,16 +1,25 @@
 package com.webProject.webProject.Store;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/store")
+@RequiredArgsConstructor
 public class StoreController {
+
+    private final StoreRepository storeRepository;
+
     @GetMapping("/list")
     public String list(Model model){
+        List<Store> storeList = this.storeRepository.findAll();
+        model.addAttribute("storeList", storeList);
         return "store_list";
     }
 
