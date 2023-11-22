d7ce1d07c933aed7fc71a062646118b17c77e18f
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-22 17:08:02 +0900

    review_form 기능 및 UI 개발

--- src/main/java/com/webProject/webProject/Review/ReviewForm.java
+++ src/main/java/com/webProject/webProject/Review/ReviewForm.java
@@ -9,12 +9,11 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 class ReviewForm {
-    private double rating;
-
     @NotEmpty(message = "내용을 입력하세요.")
     private String content;
 
-    private LocalDateTime createDate;
+    @NotEmpty(message = "평점을 입력하세요.")
+    private double rating;
 
 //    private List<MultipartFile> images; //댓글 등록할때 이미지 첨부 받아오는 속성.. 추후수정예정 잘 몰라서 ㅠㅜ
 }
