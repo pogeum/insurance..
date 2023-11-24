4befd469a179361b1c1dc4d06cda53730ff65e34
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-24 16:35:38 +0900

    수정

--- src/main/java/com/webProject/webProject/Photo/PhotoRepository.java
+++ src/main/java/com/webProject/webProject/Photo/PhotoRepository.java
@@ -1,6 +1,12 @@
 package com.webProject.webProject.Photo;
 
+import com.webProject.webProject.Review.Review;
+import com.webProject.webProject.Review_tag.Review_tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    List<Photo> findByReviewId(Integer reviewId);
 }
--- src/main/java/com/webProject/webProject/Review/ReviewRepository.java
+++ src/main/java/com/webProject/webProject/Review/ReviewRepository.java
@@ -2,10 +2,10 @@ package com.webProject.webProject.Review;
 
 import com.webProject.webProject.Store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-    List<Review> findBystoreId(Integer restaurant);
     List<Review> findAllByStore(Store store);
 }
