0ea2a213ab5b2f756ee985edbd1a5efc7760b111
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-27 16:19:09 +0900

    각 사장의 게시글의 사장님 답글 표현 기능 개발

--- src/main/java/com/webProject/webProject/Store/StoreForm.java
+++ src/main/java/com/webProject/webProject/Store/StoreForm.java
@@ -1,5 +1,6 @@
 package com.webProject.webProject.Store;
 
+import com.webProject.webProject.Menu.Menu;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,7 +15,7 @@ public class StoreForm {
     private String content; //설명 없을시 설명이 없습니다. 라고 뜨도록?
     private String category;
     private String roadAddress;
-
+    private List<Menu> menuList;
     private List<MultipartFile> fileList; //    사장이 등록할 사진
 
 }
